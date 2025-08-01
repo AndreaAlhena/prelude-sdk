a347fad38782812cd7fbf55884767f2bb4a3ca4b
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-08-01 18:18:41 +0200

    feat(enums): add TargetType and SignalDevicePlatform enums
    Add new enum classes for target types (email/phone) and signal device platforms (android/ios/web etc) to standardize these values across the codebase

---
+++ src/Enums/SignalDevicePlatform.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Prelude\SDK\Enums;
+
+enum SignalDevicePlatform: string
+{
+    case ANDROID = 'android';
+    case IPADOS = 'ipados';
+    case IOS = 'ios';
+    case TVOS = 'tvos';
+    case WEB = 'web';
+}
---
+++ src/Enums/TargetType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Prelude\SDK\Enums;
+
+enum TargetType: string
+{
+    case EMAIL_ADDRESS = 'email_address';
+    case PHONE_NUMBER = 'phone_number';
+}
