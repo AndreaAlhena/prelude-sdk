ecec75ce3af8830e93558b681f2a679ed261dfd5
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-08-01 21:14:51 +0200

    feat(Options): add method and locale enums and support in Options class
    Add new OptionsMethod and OptionsLocale enums to support different delivery methods and locales.
    Extend Options class to include these new parameters in the toArray output when provided.

---
+++ src/Enums/OptionsMethod.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Prelude\SDK\Enums;
+
+enum OptionsMethod: string
+{
+    case AUTO = 'auto';
+    case VOICE = 'voice';
+}
--- src/ValueObjects/Options.php
+++ src/ValueObjects/Options.php
@@ -2,20 +2,35 @@
 
 namespace Prelude\SDK\ValueObjects;
 
+use Prelude\SDK\Enums\OptionsMethod;
+use Prelude\SDK\Enums\OptionsLocale;
+
 class Options
 {
     public function __construct(
         private string $_templateId,
-        private array $_variables = []
+        private array $_variables = [],
+        private ?OptionsMethod $_method = null,
+        private ?OptionsLocale $_locale = null
     ) {
         //
     }
 
     public function toArray(): array
     {
-        return [
+        $result = [
             'template_id' => $this->_templateId,
             'variables' => $this->_variables,
         ];
+
+        if ($this->_method !== null) {
+            $result['method'] = $this->_method->value;
+        }
+
+        if ($this->_locale !== null) {
+            $result['locale'] = $this->_locale->value;
+        }
+
+        return $result;
     }
 }
