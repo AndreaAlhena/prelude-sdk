0fa4fff60cd132b528bdcd7c82ac2e1180f48880
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-08-02 04:32:48 +0200

    feat(config): add Config class with SDK constants and defaults

---
+++ src/Config/Config.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Prelude\SDK\Config;
+
+/**
+ * Configuration class for Prelude SDK
+ */
+class Config
+{
+    public const DEFAULT_BASE_URL = 'https://api.prelude.com';
+    public const DEFAULT_TIMEOUT = 30;
+    public const DEFAULT_USER_AGENT = 'Prelude-PHP-SDK/1.0.0';
+    
+    // API endpoints
+    public const ENDPOINT_VERIFICATION = '/v2/verification';
+    public const ENDPOINT_VERIFICATION_CHECK = '/v2/verification/check';
+    
+    // Default verification options
+    public const DEFAULT_CODE_LENGTH = 6;
+    public const DEFAULT_EXPIRY_MINUTES = 10;
+    public const DEFAULT_MAX_ATTEMPTS = 3;
+    
+
+    
+    /**
+     * Get default verification options
+     * 
+     * @return array
+     */
+    public static function getDefaultVerificationOptions(): array
+    {
+        return [
+            'code_length' => self::DEFAULT_CODE_LENGTH,
+            'expiry_minutes' => self::DEFAULT_EXPIRY_MINUTES,
+            'max_attempts' => self::DEFAULT_MAX_ATTEMPTS
+        ];
+    }
+}
